c717e3dd45ab4443202a0ebccc982d30e13f90c4
NRAAA <nrartadi@gmail.com>
2020-09-24 19:54:16 -0500

    Proyecto Base

---
+++ mjr-api/src/main/java/com/mjr/mjrapi/MjrApiApplication.java
@@ -0,0 +1,13 @@
+package com.mjr.mjrapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MjrApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MjrApiApplication.class, args);
+	}
+
+}
---
+++ mjr-api/src/test/java/com/mjr/mjrapi/MjrApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mjr.mjrapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MjrApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
