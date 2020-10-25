be3fe9e6a28beae8cd743e9534f4245c50239b74
NRAAA <nrartadi@gmail.com>
2020-10-25 06:05:09 -0500

    Fixed Lombok's ToString + Connection Properties

--- mjr-api/src/test/java/com/mjr/ClassesRepositoryTests.java
+++ mjr-api/src/test/java/com/mjr/ClassesRepositoryTests.java
@@ -1,19 +1,11 @@
 package com.mjr;
 
-import com.mjr.model.ClassStudents;
-import com.mjr.model.ClassToTeach;
-import com.mjr.model.Teacher;
-import com.mjr.repository.ClassStudentsRepository;
 import com.mjr.repository.ClassToTeachRepository;
-import com.mjr.repository.TeacherRepository;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.test.context.SpringBootTest;
 
-import java.time.Instant;
-import java.time.LocalDate;
-import java.util.Date;
+import java.sql.Date;
 
 @SpringBootTest
 public class ClassesRepositoryTests {
@@ -21,6 +13,6 @@ public class ClassesRepositoryTests {
     ClassToTeachRepository classToTeachRepository;
     @Test
     void tryGetClassesByTeacherAndDateRage(){
-        System.out.print(classToTeachRepository.getAllClassesByTeacherAndDate(1, java.sql.Date.valueOf("1999-09-09"), java.sql.Date.valueOf("2021-07-09")));
+        System.out.print(classToTeachRepository.getAllClassesByTeacherAndDate(1, Date.valueOf("1999-09-09"), Date.valueOf("2021-07-09")));
     }
 }
